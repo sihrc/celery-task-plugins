090824ab9d3c621020bdd91533faec13d434466e
Chris Lee <sihrc.c.lee@gmail.com>
2019-06-02 14:47:21 -0400

    FIX: add submodule packages

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import find_packages, setup
 
 redis_chain_store_requires = ["redis==3.2.1", "hiredis==1.0.0"]
 
@@ -9,10 +9,10 @@ setup(
     description="Celery task plugins that wrap celery tasks",
     author="Chris Lee",
     author_email="sihrc.c.lee@gmail.com",
-    packages=["celery_task_plugins"],
+    packages=find_packages(),
     install_requires=["celery>=4.3.0"],
     extras_require={
-        "redis_chain_store_requires": redis_chain_store_requires,
+        "redis_chain_store": redis_chain_store_requires,
         "test": ["pytest"] + redis_chain_store_requires,
     },
 )
